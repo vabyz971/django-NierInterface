803c808ebd6e5fa0fcd065ad8f1296242739e1ee
Vabyz971 <vabyz971@gmail.com>
2020-09-16 14:26:02 -0400

    Suppréssion du model

--- nierInterface/admin.py
+++ nierInterface/admin.py
@@ -2,29 +2,5 @@
 # @Date:   2020-09-03T11:45:06-04:00
 # @Email:  vabyz971@gmail.com
 # @Last modified by:   jahleel
-# @Last modified time: 2020-09-03T13:51:40-04:00
+# @Last modified time: 2020-09-16T14:23:05-04:00
 # @License: GPLv3
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from django.contrib.sites.models import Site
-from django.contrib.sites.admin import SiteAdmin as DefaultSiteAdmin
-from django.utils.translation import gettext_lazy as _
-
-from .models import SiteConfig
-from .forms import SiteConfigForm
-
-
-class SiteConfigInline(admin.StackedInline):
-    form = SiteConfigForm
-    model = SiteConfig
-    can_delete = False
-    verbose_name_plural = _('site configuration')
-
-
-class SiteAdmin(DefaultSiteAdmin):
-    inlines = [SiteConfigInline]
-
-
-admin.site.unregister(Site)
-admin.site.register(Site, SiteAdmin)
